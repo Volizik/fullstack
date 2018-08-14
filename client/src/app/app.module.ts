fdda940ab319018feb9835c62260c5337254fdfc
Volizik <kizilov.yura@gmail.com>
2018-08-14 23:51:28 +0300

    add front part

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,55 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppComponent} from './app.component';
+import {LoginPageComponent} from './login-page/login-page.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {AuthLayoutComponent} from './shared/layouts/auth-layout/auth-layout.component';
+import {MainLayoutComponent} from './shared/layouts/main-layout/main-layout.component';
+import {RegisterPageComponent} from './register-page/register-page.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {TokenInterceptor} from "./shared/classes/token.interceptor";
+import { OverviewPageComponent } from './overview-page/overview-page.component';
+import { AnalyticsPageComponent } from './analytics-page/analytics-page.component';
+import { HistoryPageComponent } from './history-page/history-page.component';
+import { OrderPageComponent } from './order-page/order-page.component';
+import { CategoriesPageComponent } from './categories-page/categories-page.component';
+import { LoaderComponent } from './shared/components/loader/loader.component';
+import { CategoriesFormComponent } from './categories-page/categories-form/categories-form.component';
+import { PositionsFormComponent } from './categories-page/categories-form/positions-form/positions-form.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        LoginPageComponent,
+        AuthLayoutComponent,
+        MainLayoutComponent,
+        RegisterPageComponent,
+        OverviewPageComponent,
+        AnalyticsPageComponent,
+        HistoryPageComponent,
+        OrderPageComponent,
+        CategoriesPageComponent,
+        LoaderComponent,
+        CategoriesFormComponent,
+        PositionsFormComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        AppRoutingModule
+    ],
+    providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            multi: true,
+            useClass: TokenInterceptor
+        }
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
