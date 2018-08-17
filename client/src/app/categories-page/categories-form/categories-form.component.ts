548bade6d6f13eb477dc908893265842e8ea415f
Volizik <kizilov.yura@gmail.com>
2018-08-17 07:21:06 +0300

    add front part

--- client/src/app/categories-page/categories-form/categories-form.component.ts
+++ client/src/app/categories-page/categories-form/categories-form.component.ts
@@ -1,10 +1,11 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Params} from "@angular/router";
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {ActivatedRoute, Params, Router} from "@angular/router";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {CategoriesService} from "../../shared/services/categories.service";
 import {switchMap} from "rxjs/operators";
 import {of} from "rxjs";
 import {MaterialService} from "../../shared/classes/material.service";
+import {Category} from "../../shared/interfaces";
 
 @Component({
     selector: 'app-categories-form',
@@ -15,9 +16,14 @@ export class CategoriesFormComponent implements OnInit {
 
     isNew = true;
     form: FormGroup;
+    image: File;
+    imagePreview = '';
+    category: Category;
+    @ViewChild('input') inputRef: ElementRef;
 
     constructor(private route: ActivatedRoute,
-                private categoriesService: CategoriesService) {
+                private categoriesService: CategoriesService,
+                private router: Router) {
     }
 
     ngOnInit() {
@@ -36,11 +42,13 @@ export class CategoriesFormComponent implements OnInit {
                 return of(null)
             })
         ).subscribe(
-            category => {
+            (category: Category) => {
                 if (category) {
+                    this.category = category;
                     this.form.patchValue({
                         name: category.name
                     });
+                    this.imagePreview = category.imgSrc;
                     MaterialService.updateTextInputs();
                 }
                 this.form.enable();
@@ -52,7 +60,56 @@ export class CategoriesFormComponent implements OnInit {
     }
 
     onSubmit() {
+        let obs$;
+        this.form.disable();
 
+        if (this.isNew) {
+            //create
+            obs$ = this.categoriesService.create(this.form.value.name, this.image);
+        } else {
+            //update
+            obs$ = this.categoriesService.update(this.category._id, this.form.value.name, this.image)
+        }
+
+        obs$.subscribe(
+            category => {
+                this.category = category;
+                this.form.enable();
+                MaterialService.toast('Изменения сохранены');
+            },
+            error => {
+                MaterialService.toast(error.message.message);
+                this.form.enable();
+            }
+        )
+    }
+
+    triggerClick() {
+        this.inputRef.nativeElement.click();
+    }
+
+    onFileUpload(event: any) {
+        const file = event.target.files[0];
+        this.image = file;
+
+        const reader = new FileReader();
+        reader.onload = () => {
+            this.imagePreview = reader.result
+        };
+        reader.readAsDataURL(file);
+    }
+
+    deleteCategory() {
+        const decision = window.confirm(`Вы уверены, что хотите удалить категорию "${this.category.name}"`);
+
+        if (decision) {
+            this.categoriesService.delete(this.category._id)
+                .subscribe(
+                    response => MaterialService.toast(response.message),
+                    err => MaterialService.toast(err.error.message),
+                    () => this.router.navigate(['/categories'])
+                )
+        }
     }
 
 }
