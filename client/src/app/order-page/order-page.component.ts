5774f1426026dd4260e80e0ed7633cac5a5d44e2
Volizik <kizilov.yura@gmail.com>
2018-08-28 07:22:55 +0300

    add categories

--- client/src/app/order-page/order-page.component.ts
+++ client/src/app/order-page/order-page.component.ts
@@ -1,15 +1,81 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {NavigationEnd, Router} from "@angular/router";
+import {MaterialInstance, MaterialService} from "../shared/classes/material.service";
+import {OrderService} from "./order.service";
+import {Order, OrderPosition} from "../shared/interfaces";
+import {OrdersService} from "../shared/services/orders.service";
+import {Subscription} from "rxjs";
 
 @Component({
-  selector: 'app-order-page',
-  templateUrl: './order-page.component.html',
-  styleUrls: ['./order-page.component.css']
+    selector: 'app-order-page',
+    templateUrl: './order-page.component.html',
+    styleUrls: ['./order-page.component.css'],
+    providers: [OrderService]
 })
-export class OrderPageComponent implements OnInit {
+export class OrderPageComponent implements OnInit, OnDestroy, AfterViewInit {
 
-  constructor() { }
+    isRoot: boolean;
+    @ViewChild('modal') modalRef: ElementRef;
+    modal: MaterialInstance;
+    pending = false;
+    onSub: Subscription;
 
-  ngOnInit() {
-  }
+    constructor(private router: Router, public order: OrderService, private ordersService: OrdersService) {
+    }
+
+    ngOnInit() {
+        this.isRoot = this.router.url === '/order';
+        this.router.events.subscribe(event => {
+            if (event instanceof NavigationEnd) {
+                this.isRoot = this.router.url === '/order';
+            }
+        })
+    }
+
+    ngOnDestroy() {
+        this.modal.destroy();
+        if (this.onSub) {
+            this.onSub.unsubscribe();
+        }
+    }
+
+    ngAfterViewInit() {
+        this.modal = MaterialService.initModal(this.modalRef);
+    }
+
+    open() {
+        this.modal.open();
+    }
+
+    cancel() {
+        this.modal.close();
+    }
+
+    submit() {
+        this.pending = true;
+        const order: Order = {
+            list: this.order.list.map(item => {
+                delete item._id;
+                return item;
+            })
+        };
+        this.onSub = this.ordersService.create(order).subscribe(
+            newOrder => {
+                MaterialService.toast(`Заказ №${newOrder.order} был добавлен.`);
+                this.order.clear();
+            },
+            error => {
+                MaterialService.toast(error.error.message)
+            },
+            () => {
+                this.modal.close();
+                this.pending = true;
+            }
+        )
+    }
+
+    removePosition(orderPosition: OrderPosition) {
+        this.order.remove(orderPosition);
+    }
 
 }
--- client/src/app/shared/interfaces.ts
+++ client/src/app/shared/interfaces.ts
@@ -13,3 +13,27 @@ export interface Category {
     user?: string,
     _id?: string
 }
+
+export interface Position {
+    name: string,
+    cost: number,
+    category: string,
+    user?: string,
+    _id?: string,
+    quantity?: number
+}
+
+export interface Order {
+    date?: Date,
+    order?: number,
+    user?: string,
+    list: OrderPosition[],
+    _id?: string
+}
+
+export interface OrderPosition {
+    name: string,
+    cost: number,
+    quantity: number,
+    _id?: string
+}
